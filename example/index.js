5d02943f6a9aed38c074521c2736ea3154c74b40
Ionică Bizău <bizauionica@yahoo.com>
2015-05-17 07:40:55 +0300

    Added example

---
+++ example/index.js
@@ -0,0 +1,45 @@
+// Dependencies
+var JsonRequest = require("../lib")
+  , Lien = require("lien")
+  ;
+
+// Init lien server
+var server = new Lien({
+    host: "localhost"
+  , port: 9000
+});
+
+// Handle "/"
+server.page.add(/^\/$/, function (lien) {
+    lien.end({
+        Hello: "World"
+    });
+})
+
+// Handle "/hello"
+server.page.add(/^\/hello$/, function (lien) {
+    lien.end({
+        method: lien.method
+      , data: lien.data
+    });
+});
+
+server.on("load", function (err) {
+    if (err) { throw err; }
+    JsonRequest("http://localhost:9000/", function (err, data) {
+        console.log(err || data);
+        JsonRequest({
+            url: "http://localhost:9000/hello"
+          , method: "POST"
+          , data: {
+                "some": "data"
+            }
+        }, function (err, data) {
+            console.log(err || data);
+            JsonRequest("https://status.github.com/api.json", function (err, data) {
+                console.log(err || data);
+                process.exit(0);
+            })
+        });
+    });
+});
