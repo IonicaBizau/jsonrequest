f79b2c7918bb2cf210d76350077db7268e0ca97f
Ionică Bizău <bizauionica@gmail.com>
2017-08-07 21:13:37 +0530

    Support for promises

--- example/index.js
+++ example/index.js
@@ -1,17 +1,28 @@
 // Dependencies
-var request = require("../lib");
+const request = require("../lib");
+
+request({
+    url: "https://api.github.com/users/IonicaBizau",
+    headers: {
+        "User-Agent": "JsonRequest"
+    }
+}).then(data => {
+    console.log(data);
+    // => {
+    //   "login": "IonicaBizau",
+    //   "id": 2864371,
+    //   "avatar_url": "https://avatars.githubusercontent.com/u/2864371?v=3",
+    //   "gravatar_id": "",
+    //   "url": "https://api.github.com/users/IonicaBizau",
+    //   "html_url": "https://github.com/IonicaBizau",
+    //   "followers_url": "https://api.github.com/users/IonicaBizau/followers",
+    //   ...
+    // }
+}).catch(err => {
+    console.error(err);
+});
 
 // Make a request to GitHub API
-request("https://api.github.com/users/IonicaBizau", function (err, data) {
+request("https://ionicabizau.net/api/articles", (err, data) => {
     console.log(err || data);
 });
-// => {
-//   "login": "IonicaBizau",
-//   "id": 2864371,
-//   "avatar_url": "https://avatars.githubusercontent.com/u/2864371?v=3",
-//   "gravatar_id": "",
-//   "url": "https://api.github.com/users/IonicaBizau",
-//   "html_url": "https://github.com/IonicaBizau",
-//   "followers_url": "https://api.github.com/users/IonicaBizau/followers",
-//   ...
-// }
